33e026636537c9d7081e3d7e1aa3632e1e40e2e6
Ned <nedalturas527@gmail.com>
2026-04-25 15:24:33 +0400

    fetch auth

--- netlify/functions/get-authors.js
+++ netlify/functions/get-authors.js
@@ -1,14 +1,20 @@
-const {admin} = require('@netlify/identity');
-
-exports.handler = async () => {
-    try{
-        const users = await admin.listUsers();
-        const authors = users.map(user => ({
-            name: user.user_metadata.full_name || "Anonymouse",
-            id: user.id
-        }));
-        return {statusCode: 200, body: JSON.stringify(authors)};
-    } catch(error){
-        return {statusCode: 500, body:error.toString()};
+// netlify/functions/get-authors.js
+exports.handler = async (event, context) => {
+    const { identity } = context.clientContext;
+  
+    try {
+      const res = await fetch(`${identity.url}/admin/users`, {
+        headers: {
+          Authorization: `Bearer ${identity.token}`
+        }
+      });
+      const data = await res.json();
+      const authors = data.users.map(user => ({
+        name: user.user_metadata.full_name || "Anonymous",
+        id: user.id
+      }));
+      return { statusCode: 200, body: JSON.stringify(authors) };
+    } catch (error) {
+      return { statusCode: 500, body: error.toString() };
     }
-}
+  };
