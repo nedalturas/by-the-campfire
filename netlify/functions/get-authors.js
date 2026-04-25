9696a5ca7a91ff52110d06d68d276ac2ec01ecda
Ned <nedalturas527@gmail.com>
2026-04-25 15:44:51 +0400

    added gravatar and testing

--- netlify/functions/get-authors.js
+++ netlify/functions/get-authors.js
@@ -1,20 +1,23 @@
-// netlify/functions/get-authors.js
+const crypto = require('crypto');
+
 exports.handler = async (event, context) => {
-    const { identity } = context.clientContext;
-  
-    try {
-      const res = await fetch(`${identity.url}/admin/users`, {
-        headers: {
-          Authorization: `Bearer ${identity.token}`
-        }
-      });
-      const data = await res.json();
-      const authors = data.users.map(user => ({
+  const { identity } = context.clientContext;
+
+  try {
+    const res = await fetch(`${identity.url}/admin/users`, {
+      headers: { Authorization: `Bearer ${identity.token}` }
+    });
+    const data = await res.json();
+    const authors = data.users.map(user => {
+      const hash = crypto.createHash('md5').update(user.email.trim().toLowerCase()).digest('hex');
+      return {
         name: user.user_metadata.full_name || "Anonymous",
+        avatar: `https://www.gravatar.com/avatar/${hash}?d=identicon`,
         id: user.id
-      }));
-      return { statusCode: 200, body: JSON.stringify(authors) };
-    } catch (error) {
-      return { statusCode: 500, body: error.toString() };
-    }
-  };
+      };
+    });
+    return { statusCode: 200, body: JSON.stringify(authors) };
+  } catch (error) {
+    return { statusCode: 500, body: error.toString() };
+  }
+};
