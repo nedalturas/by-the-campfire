3e1a9d7e26eeab4c2a9ccdfecc5d0c451f03ccd2
nedalturas <nedalturas527@gmail.com>
2026-04-27 00:54:59 +0800

    robots

--- .eleventy.js
+++ .eleventy.js
@@ -8,6 +8,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/css");
   eleventyConfig.addPassthroughCopy("src/admin");
   eleventyConfig.addPassthroughCopy("src/images");
+  eleventyConfig.addPassthroughCopy("src/robots.txt");
 
   // Convert Markdown stored in front matter to HTML
   eleventyConfig.addFilter("markdownify", (content = "") => {
